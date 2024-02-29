693fa9b7063ac6d6aa6bc0101efb502a2ddb4376
RomLabo <82595594+RomLabo@users.noreply.github.com>
2024-02-29 23:03:27 +0100

    suite refactoring view et presenter

---
+++ js/JForm.js
@@ -0,0 +1,191 @@
+/*
+0000000001 Author RomLabo 111111111
+1000111000 Class JForm 111111111111
+1000000001 Created on 17/04/2023 11
+10001000111110000000011000011100001
+10001100011110001100011000101010001
+00000110000110000000011000110110001
+*/
+
+/**
+ * @class JForm
+ * @description ...
+ */
+class JForm {
+    /**
+     * 
+     * @param {*} formWrapper 
+     */
+    constructor(formWrapper) {
+        this.wrapper = formWrapper;
+        this.form = document.getElementById('form');
+        this.inputWrapper = document.getElementById('input-wrapper');
+        this.addInputBtn = document.getElementById('add-input');
+        this.removeInputBtn = document.getElementById('remove-input');
+        this.inputs = this.inputWrapper.children;
+        this._inputsData = [];
+        this.allInputs;
+        this._currentType;
+        this.modifyForm();
+    }
+
+    get inputsData() {
+        this._inputsData = [];
+        for (let i = 0; i < this.inputs.length; i++) {
+            this._inputsData.push(this.inputs[i].value);
+        }
+        if (this._currentType === TYPE.SWITCH) {
+            this._inputsData.push(document.getElementById("inp_0").value)
+        }
+        return this._inputsData;
+    }
+
+    addTextInput(txtArray, type) {
+        this._currentType = type;
+        if (type === TYPE.SWITCH) {
+            for (let i = 0; i < txtArray.length - 1; i++) {
+                this.inputWrapper.innerHTML += `<textarea class="model-input" id="inp_${this.inputs.length + 1}"
+                                            cols="20" rows="6" placeholder="..."></textarea>`;
+                document.getElementById("inp_0").style.display = "flex";                                            
+            }
+        } else {
+            for (let i = 0; i < txtArray.length; i++) {
+                this.inputWrapper.innerHTML += `<textarea class="model-input" id="inp_${this.inputs.length + 1}"
+                                            cols="20" rows="6" placeholder="..."></textarea>`;
+            }
+        }
+        for (let i = 0; i < txtArray.length; i++) {
+            if (type === TYPE.ISSUE && (i === 0 || i === 2)) {
+                this.inputs[i].value = txtArray[i].join(' ').replaceAll('  ', ' ');
+            } else if (type === TYPE.SWITCH) {
+                if (i !== txtArray.length - 1) {
+                    this.inputs[i].value = txtArray[i].join('\n');    
+                } else {
+                    document.getElementById("inp_0").value = txtArray[i].join('\n');
+                }
+                
+            } else {
+                this.inputs[i].value = txtArray[i].join('\n');
+            }
+        }
+    }
+
+    resetInputs() {
+        for (let i = 0; i < this.inputs.length; i++) {
+            this.inputs[i].value = "";
+        }
+        document.getElementById("inp_0").value = "";
+    }
+
+    modifyForm() {
+        this.addInputBtn.addEventListener('click', () => {
+            if (this.inputs.length === 1) {
+                this.removeInputBtn.removeAttribute("disabled");
+            }
+            if (this.inputs.length <= 7) {
+                if (this._currentType === TYPE.CONDITION) {
+                    if (this.inputs.length > 1) {
+                        this.inputs[this.inputs.length - 1].placeholder = "Si ... Sinon";
+                    }
+                    this.inputWrapper.innerHTML += `<textarea class="model-input" id="inp_${this.inputs.length + 1}"
+                                             cols="20" rows="6" placeholder="Sinon ..."></textarea>`;
+                } else {
+                    this.inputWrapper.innerHTML += `<textarea class="model-input" id="inp_${this.inputs.length + 1}"
+                                             cols="20" rows="6" placeholder="Valeur${this.inputs.length + 1}"></textarea>`;
+                }
+            }
+            if (this.inputs.length === 8) {
+                this.addInputBtn.setAttribute("disabled",true);
+            }
+        })
+        this.removeInputBtn.addEventListener('click', () => {
+            if (this.inputs.length === 8) {
+                this.addInputBtn.removeAttribute("disabled");
+            }
+            if (this.inputs.length > 1) {
+                let elm = document.getElementById(`${this.inputs[this.inputs.length -1].id}`);
+                elm.parentNode.removeChild(elm);
+                if (this._currentType === 204 && this.inputs.length > 1) {
+                    this.inputs[this.inputs.length - 1].placeholder = "Sinon ...";
+                }
+            }
+            if (this.inputs.length === 1) {
+                this.removeInputBtn.setAttribute("disabled",true);
+            }
+        })
+    }
+
+    hide () {
+        this.wrapper.style.zIndex = -3;
+        this.inputWrapper.innerHTML = "";
+        document.getElementById("inp_0").style.display = "none";
+    }
+
+    isValid() {
+        let isValid = false;
+        switch (this._currentType) {
+            case TYPE.ISSUE:
+                isValid = this.inputsData[1] !== "";
+                break;
+            case TYPE.ASSIGNEMENT:
+                isValid = this.inputsData[0] !== "";
+                break;
+            case TYPE.SWITCH:
+                isValid = this.inputsData.every(input => input !== "");
+                break;
+            case TYPE.LOOP:
+                isValid = true;
+                break;
+            case TYPE.CONDITION:
+                isValid = this.inputsData.every(input => input !== "");
+                break;
+            default:
+                isValid = true;
+                break;
+        }
+        return isValid;
+    }
+
+    create(type) {
+        this._currentType = type;
+        switch (type) {
+            case TYPE.ISSUE:
+                this.inputWrapper.innerHTML = `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Donnée1, ..."></textarea>
+                                    <textarea class="model-input" id="inp_2" cols="20" rows="6" placeholder="Problème"></textarea>
+                                    <textarea class="model-input" id="inp_3" cols="20" rows="6" placeholder="Résultat1, ..."></textarea>`;    
+                break;
+            case TYPE.ASSIGNEMENT:
+                this.inputWrapper.innerHTML = `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="valeur1 <-- valeur2"></textarea>`;
+                break;
+            case TYPE.SWITCH:
+                document.getElementById("inp_0").value = "";
+                document.getElementById("inp_0").style.display = "flex";
+                this.removeInputBtn.removeAttribute("disabled");
+                this.inputWrapper.innerHTML = `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Valeur1"></textarea>
+                                            <textarea class="model-input" id="inp_2" cols="20" rows="6" placeholder="Valeur2"></textarea>`;
+                
+                break;
+            case TYPE.LOOP:
+                this.inputWrapper.innerHTML = `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Pour i allant\nde ... à ..."></textarea>`;
+                break;
+            case TYPE.CONDITION:
+                this.inputWrapper.innerHTML = `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Si ..."></textarea>`;
+                break;
+            default:
+                this.inputWrapper.innerHTML = `<textarea class="model-input" disabled id="inp_1" cols="20" rows="6"></textarea>`;
+                break;
+        }
+    }
+    
+    show(type) {
+        document.getElementById("inp_1").focus();
+        if (type === TYPE.CONDITION || type === TYPE.SWITCH) {
+            this.addInputBtn.style.display = 'inline-block';
+            this.removeInputBtn.style.display = 'inline-block';
+        } else {
+            this.addInputBtn.style.display = 'none';
+            this.removeInputBtn.style.display = 'none';
+        }
+        this.wrapper.style.zIndex = 3;
+    }
+}
--- js/JView.js
+++ js/JView.js
@@ -12,20 +12,44 @@
  * @description Delegates interaction to the presenter.
  */
 class JView {
+    // Private properties
+    #presenter; #dataForm;
+    #inputsDefaultTxt; #inputsElms;
     /**
-     * Create a JView.
+     * * @description Create a JView.
      */
     constructor() {
-        this._presenter = null;
+        this.#presenter = null;
+
+        /* A Modifier ----------- */
         document.getElementById("main-canvas").width = window.innerWidth * .98;
         document.getElementById("main-canvas").height = window.innerHeight * .9;
+        /* -------------------- */
+
+        this.#dataForm = [];
+        this.#inputsDefaultTxt = {
+            issue: "Problème", assignment: "Valeur1 <-- Valeur2",
+            switch: "Valeur", loop: "Pour i allant\nde ... à ...",
+            condition: "Si ..."
+        };
+        this.#inputsElms = {
+            issue: `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Donnée1, ..."></textarea>
+                    <textarea class="model-input" id="inp_2" cols="20" rows="6" placeholder="Problème"></textarea>
+                    <textarea class="model-input" id="inp_3" cols="20" rows="6" placeholder="Résultat1, ..."></textarea>`,
+            assignment: `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Valeur1 <-- Valeur2"></textarea>`,
+            switch: `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Valeur1"></textarea>
+                    <textarea class="model-input" id="inp_2" cols="20" rows="6" placeholder="Valeur2"></textarea>`,
+            loop: `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Pour i allant\nde ... à ..."></textarea>`,
+            condition: `<textarea class="model-input" id="inp_1" cols="20" rows="6" placeholder="Si ..."></textarea>`
+        };
     }
 
-    set presenter(val) { this._presenter = val }
-    get presenter() { return this._presenter }
+    set presenter(val) { this.#presenter = val }
+    get presenter() { return this.#presenter }
 
     /**
-     * Removes the bidirectional relationship with the presenter.
+     * @description Removes the bidirectional relationship 
+     * with the presenter.
      */
     unlinkPresenter() {
         if (this.presenter !== null) {
@@ -35,7 +59,8 @@ class JView {
     }
 
     /**
-     * Create a bidirectional relationship with the presenter.
+     * @description Create a bidirectional relationship 
+     * with the presenter.
      * @param {JPresenter} presenter 
      */
     linkPresenter(presenter) {
@@ -44,16 +69,190 @@ class JView {
             presenter.unlinkView();
             this.presenter = presenter;
             presenter.view = this;
-            this.lauchClickListener();
+            this.launchClickListener();
+            this.launchMouseMoveListener();
         }
     }
 
     /**
+     * @description Display landmarks.
+     */
+    displayLandmarks(e) {
+        document.getElementById("landmark-v").style.left = `${(e.clientX)}px`;
+        document.getElementById("landmark-h").style.top = `${(e.offsetY)}px`;
+        document.getElementById("landmark-v").style.opacity = 1;
+        document.getElementById("landmark-h").style.opacity = 1;
+    }
+
+    /**
+     * @description Hides landmarks.
+     */
+    hideLandmarks() {
+        document.getElementById("landmark-v").style.opacity = 0;
+        document.getElementById("landmark-h").style.opacity = 0;
+    }
+
+    /**
      * 
+     */
+    displayInputsControls() {
+        document.getElementById("remove-input").style.display = "inline-block";
+        document.getElementById("add-input").style.display = "inline-block";
+    }
+
+    /**
+     * 
+     */
+    hideInputsControls() {
+        document.getElementById("remove-input").style.display = "none";
+        document.getElementById("add-input").style.display = "none";
+    }
+
+    /**
+     * @description Enable the new button
+     * * of main menu.
+     */
+    enableNewBtn() {
+        document.getElementById("main-new")
+                .removeAttribute("disabled");
+    }
+
+    /**
+     * @description Disable the new button
+     * * of main menu.
+     */
+    disableNewBtn() {
+        document.getElementById("main-new")
+                .setAttribute("disabled", true);
+    }
+
+    /**
+     * @description Enable the undo button
+     * * of main menu.
+     */
+    enableUndoBtn() {
+        document.getElementById("main-undo")
+                .removeAttribute("disabled");
+    }
+
+    /**
+     * @description Disable the undo button
+     * * of main menu.
+     */
+    disableUndoBtn() {
+        document.getElementById("main-undo")
+                .setAttribute("disabled", true);
+    }
+
+    /**
+     * @description Enable the redo button
+     * * of main menu.
+     */
+    enableRedoBtn() {
+        document.getElementById("main-redo")
+                .removeAttribute("disabled");
+    }
+
+    /**
+     * @description Disable the redo button
+     * of main menu.
+     */
+    disableRedoBtn() {
+        document.getElementById("main-redo")
+                .setAttribute("disabled", true);
+    }
+
+    /**
+     * @description Enable the breakdown button 
+     * of node menu.
+     */
+    enableBreakDownBtn() {
+        document.getElementById("node-breakdown")
+                .removeAttribute("disabled");
+    }
+
+    /**
+     * @description Disable the breakdown button
+     * of node menu.
+     */
+    disableBreakDownBtn() {
+        document.getElementById("node-breakdown")
+                .setAttribute("disabled", true);
+    }
+
+    /**
+     * @description displays the type menu 
+     * for the node.
+     */
+    displayTypeMenu() {
+        document.getElementById("type__nav").style.zIndex = 5;
+    }
+
+    /**
+     * @description hides the type menu for
+     * the node.
+     */
+    hideTypeMenu() {
+        document.getElementById("type__nav").style.zIndex = -5;
+    }
+
+    /**
+     * @description Builds the node's text 
+     * modification form.
+     * @param {String} type 
+     */
+    buildForm(type) {
+        if (type === "condition" || type === "switch") {
+            this.displayInputsControls();
+        } else {
+            this.hideInputsControls();
+        }
+        document.getElementById("input-wrapper")
+                    .innerHTML = "";
+        document.getElementById("input-wrapper")
+                    .innerHTML = this.#inputsElms[type];
+    }
+
+    /**
+     * @description Displays the node's text 
+     * modification form.
+     */
+    displayForm() {
+        document.getElementById("inp_1").focus();
+        document.getElementById("node__form").style.zIndex = 3;
+    }
+
+    /**
+     * @description Hides the node's text 
+     * modification form.
+     */
+    hideForm () {
+        document.getElementById("node__form").style.zIndex = -3;
+        document.getElementById("inp_0").style.display = "none";
+    }
+
+    /**
+     * @description
+     * @returns 
+     */
+    getDataForm() {
+        this.#dataForm.splice(0);
+        for (let i = 0; 
+            i < document.getElementById("input-wrapper").children.length; 
+                                                                    i++) {
+            this.#dataForm.push(
+                document.getElementById("input-wrapper").children[i].value
+            )
+        }
+        return this.#dataForm;
+    }
+
+    /**
+     * @description Handles interactions with the main menu.
      * @param {String} action 
      * @param {Event} event 
      */
-    mainMenuHandler(action, event) {
+    handleMainMenu(action, event) {
         switch (action) {
             case "add": this.presenter.handleAdd(event); break;
             case "undo": this.presenter.handleUndo(event); break;
@@ -66,11 +265,11 @@ class JView {
     }
 
     /**
-     * 
+     * @description Handles interactions with the node menu.
      * @param {*} action 
      * @param {*} event 
      */
-    typeMenuHandler(action, event) {
+    handleNodeMenu(action, event) {
         switch (action) {
             case value: break;
             default: break;
@@ -78,27 +277,31 @@ class JView {
     }
 
     /**
-     * 
-     * @param {*} action 
-     * @param {*} event 
+     * @description Starts the click event listener.
      */
-    nodeMenuHandler(action, event) {
-        switch (action) {
-            case value: break;
-            default: break;
-        }
-    }
-
-    /**
-     * 
-     */
-    lauchClickListener() {
+    launchClickListener() {
         window.addEventListener("click", (e) => {
+            console.log(e.target);
             let btnData = e.target.id.split("-");
+
             switch (btnData[0]) {
-                case "main": this.mainMenuHandler(btnData[1],e); break;
-                case "type": this.typeMenuHandler(btnData[1],e); break;
-                case "node": this.nodeMenuHandler(btnData[1],e); break;
+                case "main": this.handleMainMenu(btnData[1],e); break;
+                case "type": this.presenter.handleTypeChoise(btnData[1]); break;
+                case "node": this.handleNodeMenu(btnData[1],e); break;
+                case "form": this.presenter.handleNodeForm(btnData[1]); break;
+            }
+        })
+    }
+
+    /**
+     * @description Starts the mousemove event listener
+     */
+    launchMouseMoveListener() {
+        window.addEventListener("mousemove", (e) => {
+            if (e.target.nodeName === "CANVAS") {
+                this.displayLandmarks(e);
+            } else {
+                this.hideLandmarks();
             }
         })
     }
